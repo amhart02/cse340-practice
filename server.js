1dac103440eba354a26235d7258ee48af20aa43f
amhart02 <har20010@byui.edu>
2025-04-30 22:02:22 -0600

    Did the unit 1 part 2 assignments

--- server.js
+++ server.js
@@ -1,21 +1,65 @@
 // Import express using ESM syntax
 import express from 'express';
- 
+
+// Add these imports to your existing imports
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 // Create an instance of an Express application
 const app = express();
- 
+const name = process.env.NAME;
+
+// Create __dirname and __filename variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the port number the server will listen on
+const NODE_ENV = process.env.NODE_ENV || 'production'
+const PORT = process.env.PORT || 3000;
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the views directory (where your templates are located)
+app.set('views', path.join(__dirname, 'src/views'));
+
 // Define a route handler for the root URL ('/')
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!</h1>');
+    const title = 'Home Page';
+    const content = '<h1>Welcome to the Home Page</h1><p>This is the main content of the home page.</p>';
+    res.render('index', { title, content })
 });
 
-app.get('/about-me', (req, res) => {
-    res.send('<h1>About Me</h1>');
+app.get('/about', (req, res) => {
+    const title = 'About Page';
+    const url = '/img/hamstermeme.webp'
+    res.render('about', { title, imageUrl : url })
 });
- 
-// Define the port number the server will listen on
-const PORT = process.env.PORT || 3000;
- 
+
+app.get('/contact', (req, res) => {
+    const title = 'Contact Page';
+    res.render('contact', { title })
+});
+
+/**
+ * Routes
+ */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/home.html'));
+});
+
+app.get('/page1', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/page1.html'));
+});
+
+app.get('/page2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/page2.html'));
+});
+
+
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
